ccad9f5840520b30a24d2aa10f55dad47dca9517
MohBelSoft <149050072+MohBelSoft@users.noreply.github.com>
2024-07-26 09:34:04 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -20,6 +20,7 @@ namespace WinFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Test");
+            MessageBox.Show("Test2");
         }
     }
 }
